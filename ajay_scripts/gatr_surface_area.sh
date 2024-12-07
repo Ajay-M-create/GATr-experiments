a00964b906e8982606b905fd569845dd2a237cb5
Ajay-M-create <ajay_m@mit.edu>
2024-12-07 12:56:20 -0500

    fixing surface area experiment

--- ajay_scripts/gatr_surface_area.sh
+++ ajay_scripts/gatr_surface_area.sh
@@ -7,11 +7,11 @@
 export BASEDIR="/home/ajay/GATr-experiments/tmp/gatr-experiments"
 echo "Base Directory: ${BASEDIR}"
 
-python ../scripts/symmetry_experiment.py \
+python ../scripts/surface_area_experiment.py \
     base_dir="${BASEDIR}" \
     seed=42 \
-    model=gatr_symmetry \
-    data.data_dir="${BASEDIR}/data/symmetry" \
+    model=gatr_surface_area \
+    data.data_dir="${BASEDIR}/data/surface_area" \
     data.subsample=0.05 \
     training.steps=10000 \
-    run_name=gatr_symmetry 
+    run_name=gatr_surface_area 
